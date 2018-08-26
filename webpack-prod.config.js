83e6c82caab3c2a8999bc53f6c4cefb5875a307e
Dominik Widomski <dominik@digital-detox.co.uk>
2018-08-27 00:01:50 +0100

    WIP some weird nodeModules excluding

--- webpack-prod.config.js
+++ webpack-prod.config.js
@@ -1,9 +1,21 @@
+const fs = require('fs');
 const webpack = require("webpack");
 const nodeExternals = require('webpack-node-externals');
 
+var nodeModules = {};
+fs.readdirSync('node_modules')
+    .filter(function (x) {
+        return ['.bin'].indexOf(x) === -1;
+    })
+    .forEach(function (mod) {
+        nodeModules[mod] = 'commonjs ' + mod;
+    });
+
 module.exports = {
     target: 'node',
+    // Had issues not including nodeExternals, things missing, I'm not sure what environment they run this in
     // externals: [nodeExternals()],
+    externals: nodeModules,
     
     plugins: [
         new webpack.DefinePlugin({
